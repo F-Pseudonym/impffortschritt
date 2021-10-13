0ffa4932ec41b17558305cdfa821e3bceed07c88
Florian Scheuer <coding@florian-scheuer.de>
2021-10-13 20:57:26 +0200

    Changed tweet layout

--- impffortschritt.rb
+++ impffortschritt.rb
@@ -44,7 +44,8 @@ rescue StandardError => e
 end
 
 data_7d = TSV.parse(response).without_header.take(data["index"]-6).last
-data["sum_7d"] = data_7d.to_a[1].to_i
+data["sum_7d"] = data_7d.to_a[table_index_sum].to_i
+data["full_7d"] = data_7d.to_a[table_index_people_full].to_i
 
 begin
   content = File.read('./data')
@@ -66,6 +67,10 @@ doses_per_day = (data["sum"] - data["sum_7d"])/7
 remaining_days = (required_doses-data["sum"])/doses_per_day
 target_date = Time.now + remaining_days * 24*60*60
 doses_per_second = (doses_per_day.to_f / (3600*24)).round(2)
+
+full_per_day = (data["full"] - data["full_7d"])/7
+full_remaining_days = (required_immune_persons_delta-data["full"])/full_per_day
+full_target_date = Time.now + full_remaining_days * 24*60*60
 
 progress_first = ((data["first"]/required_immune_persons_delta.to_f)*100).round(1)
 progress_full = ((data["full"]/required_immune_persons_delta.to_f)*100).round(1)
@@ -98,17 +103,16 @@ BAR_LENGTH.times { |i|
 }
 
 
-line1 = "Geimpfte Dosen (Deutschland): #{data["sum"].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse} 💉\n"
-line11 = "Herdenimmunität (85% vollst. Geimpfte): #{required_immune_persons_delta.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse} 🧑\n"
+line1 = "Geimpfte Dosen: #{data["sum"].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse} 💉\n⌀(7 Tage): #{doses_per_second} 💉/s\n"
 line2 = "Fortschritt für Herdenimmunität:\n"
 line3 = bar_full + " (#{progress_full}%)\n"
-line31 = "Fortschritt Erstgeimpfte:\n"
-line32 = bar_first + " (#{progress_first}%)\n"
-# line4 = "Geschätzes Zieldatum: #{target_date.strftime("%d.%m.%Y")} 📅\n"
-line5 = "⌀(7 Tage): #{doses_per_second} Impf./s"
+line4 = "Fortschritt Erstgeimpfte:\n"
+line5 = bar_first + " (#{progress_first}%)\n"
+line6 = "Geschätzes Zieldatum: #{full_target_date.strftime("%d.%m.%Y")} 📅\n"
 
 
-tweet = line1 + line11 + line2 + line3 + line31 + line32 +  line5
+
+tweet = line1 + line2 + line3 + line4 + line5 + line6
 
 
 
