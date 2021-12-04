8f583291f561fccaea597a26167509dee64d1d81
Florian Scheuer <coding@florian-scheuer.de>
2021-12-04 23:15:52 +0100

    updated column order again

--- impffortschritt.rb
+++ impffortschritt.rb
@@ -17,8 +17,8 @@ CHAR_N = "⬜️"
 BAR_LENGTH = 20
 
 table_index_sum = 1
-table_index_people_first = 26
-table_index_people_full = 27
+table_index_people_first = 27
+table_index_people_full = 28
 
 
 required_doses = (POPULATION * DOSES_PER_PERSON * PERCENTAGE_HERD_IMMUNITY).round(0)
